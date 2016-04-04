a86c19bc0f1c0969e3b3e80b665d0261997dffa9
Jaswin <jskohli25@gmail.com>
2016-04-04 13:21:07 -0500

    changed full stop to middot. hyphen to dash. Minor UI changes

--- app/src/main/java/com/jsk/morseprobe/MainActivity.java
+++ app/src/main/java/com/jsk/morseprobe/MainActivity.java
@@ -17,6 +17,8 @@ import android.hardware.Camera;
 
 import android.hardware.Camera.Parameters;
 import android.os.Handler;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,44 +31,44 @@ public class MainActivity extends AppCompatActivity {
 
     protected String generateCode(String message){
         Map<Character,String> morseCode = new HashMap<Character,String>();
-        morseCode.put('A', ".-");
-        morseCode.put('B', "-...");
-        morseCode.put('C',"-.-.");
-        morseCode.put('D',"-..");
-        morseCode.put('E',".");
-        morseCode.put('F',"..-.");
-        morseCode.put('G',"--.");
-        morseCode.put('H',"....");
-        morseCode.put('I',"..");
-        morseCode.put('J',".---");
-        morseCode.put('K',"-.-");
-        morseCode.put('L',".-..");
-        morseCode.put('M',"--");
-        morseCode.put('N',"-.");
-        morseCode.put('O',"---");
-        morseCode.put('P',".--.");
-        morseCode.put('Q',"--.-");
-        morseCode.put('R',"._.");
-        morseCode.put('S',"...");
-        morseCode.put('T',"-");
-        morseCode.put('U',"..-");
-        morseCode.put('V',"...-");
-        morseCode.put('W',".--");
-        morseCode.put('X',"-..-");
-        morseCode.put('Y',"-.--");
-        morseCode.put('Z',"--..");
+        morseCode.put('A', "·—");
+        morseCode.put('B', "—···");
+        morseCode.put('C',"—·—·");
+        morseCode.put('D',"—··");
+        morseCode.put('E',"·");
+        morseCode.put('F',"··—.");
+        morseCode.put('G',"——·");
+        morseCode.put('H',"····");
+        morseCode.put('I',"··");
+        morseCode.put('J',"·———");
+        morseCode.put('K',"—·—");
+        morseCode.put('L',"·—··");
+        morseCode.put('M',"——");
+        morseCode.put('N',"—·");
+        morseCode.put('O',"———");
+        morseCode.put('P',"·——·");
+        morseCode.put('Q',"——·—");
+        morseCode.put('R',"·—·");
+        morseCode.put('S',"···");
+        morseCode.put('T',"—");
+        morseCode.put('U',"··—");
+        morseCode.put('V',"···—");
+        morseCode.put('W',"·——");
+        morseCode.put('X',"—··—");
+        morseCode.put('Y',"—·—");
+        morseCode.put('Z',"——··");
 
-        morseCode.put('1',".----");
-        morseCode.put('2',"..---");
-        morseCode.put('3',"...--");
-        morseCode.put('4',"....-");
-        morseCode.put('5',".....");
-        morseCode.put('6',"-....");
-        morseCode.put('7',"--...");
-        morseCode.put('8',"---..");
-        morseCode.put('9',"----.");
-        morseCode.put('0',"-----");
-        morseCode.put(' ',"   ");
+        morseCode.put('1',"·————");
+        morseCode.put('2',"··———");
+        morseCode.put('3',"···——");
+        morseCode.put('4',"····—");
+        morseCode.put('5',"·····");
+        morseCode.put('6',"—····");
+        morseCode.put('7',"——···");
+        morseCode.put('8',"———··");
+        morseCode.put('9',"————·");
+        morseCode.put('0',"—————");
+        morseCode.put(' ',"  ");
 
         message = message.toUpperCase();
 
@@ -107,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 for(int i=0; i<message.length(); i++) {
                     switch(message.charAt(i)) {
-                        case '.':
+                        case '·':
                             params.setFlashMode(Parameters.FLASH_MODE_TORCH);
                             camera.setParameters(params);
                             camera.startPreview();
@@ -120,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
                             camera.setParameters(params);
                             camera.startPreview();
                             break;
-                        case '-':
+                        case '—':
                             params.setFlashMode(Parameters.FLASH_MODE_TORCH);
                             camera.setParameters(params);
                             camera.startPreview();
@@ -157,6 +159,67 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void playMessage(final String message) {
+        mediaPlayer = MediaPlayer.create(this,R.raw.beep);
+        threadAlive=true;
+        Thread t= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0; i<message.length(); i++) {
+                    switch(message.charAt(i)) {
+                        case '·':
+                            mediaPlayer.start();
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            if(mediaPlayer.isPlaying()) {
+                                mediaPlayer.seekTo(0);
+                                mediaPlayer.pause();
+                            }
+                            break;
+                        case '—':
+                            mediaPlayer.start();
+                            try {
+                                Thread.sleep(300);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            if(mediaPlayer.isPlaying()) {
+                                mediaPlayer.seekTo(0);
+                                mediaPlayer.pause();
+                            }
+                            break;
+                        case ' ':
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            if(mediaPlayer.isPlaying()) {
+                                mediaPlayer.seekTo(0);
+                                mediaPlayer.pause();
+                            }
+                            break;
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                threadAlive=false;
+            }
+        });
+
+        t.start();
+        if(!threadAlive) {
+            t.interrupt();
+            t=null;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -164,10 +227,10 @@ public class MainActivity extends AppCompatActivity {
 
         Button generateCodeButton = (Button) findViewById(R.id.button);
         Button flashCodeButton = (Button) findViewById(R.id.button2);
-        Button playBeep = (Button) findViewById(R.id.button3);
+        final Button playBeep = (Button) findViewById(R.id.button3);
         final EditText editText = (EditText) findViewById(R.id.message);
         final TextView codedTextView = (TextView) findViewById(R.id.codedMessage);
-        mediaPlayer = MediaPlayer.create(this,R.raw.beep);
+
 
         generateCodeButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -203,15 +266,19 @@ public class MainActivity extends AppCompatActivity {
                             }
                             else {
                                 String message = editText.getText().toString();
-                                String codedMessage = generateCode(message);
-                                if(codedMessage != null) {
-                                    Toast.makeText(MainActivity.this, "Flashing Morse Code!", Toast.LENGTH_SHORT).show();
-                                    codedTextView.setText(codedMessage);
-                                    flashMessage(codedMessage);
-
+                                if(message==null) {
+                                    Toast.makeText(MainActivity.this,"No value entered!",Toast.LENGTH_SHORT).show();
                                 }
                                 else {
-                                    Toast.makeText(MainActivity.this, "ERROR: message must contain only characters (A-Z) and numbers(0-9)", Toast.LENGTH_LONG).show();
+                                    String codedMessage = generateCode(message);
+                                    if (codedMessage != null) {
+                                        Toast.makeText(MainActivity.this, "Flashing Morse Code!", Toast.LENGTH_SHORT).show();
+                                        codedTextView.setText(codedMessage);
+                                        flashMessage(codedMessage);
+
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "ERROR: message must contain only characters (A-Z) and numbers(0-9)", Toast.LENGTH_LONG).show();
+                                    }
                                 }
                             }
                         }
@@ -223,7 +290,20 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mediaPlayer.start();
+                        String message = editText.getText().toString();
+                        if(message==null) {
+                            Toast.makeText(MainActivity.this,"No value entered!",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            String codedMessage = generateCode(message);
+                            if (codedMessage != null) {
+                                Toast.makeText(MainActivity.this, "Playing Morse Code!", Toast.LENGTH_SHORT).show();
+                                codedTextView.setText(codedMessage);
+                                playMessage(codedMessage);
+                            } else {
+                                Toast.makeText(MainActivity.this, "ERROR: message must contain only characters (A-Z) and numbers(0-9)", Toast.LENGTH_LONG).show();
+                            }
+                        }
                     }
                 }
         );
